841c99bb7d86ff033605babee306f4d0531cdb08
LAPTOP-7U2RL3VE\WilliamLuo <jackluotong@outlook.com>
2020-12-03 18:08:09 +0800

    complete this project change some procedure.

--- App.js
+++ App.js
@@ -1,12 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-// import Vant from 'vant';
-// import 'vant/lib/index.css';
 import React from 'react';
 import {
   SafeAreaView,
@@ -15,83 +6,229 @@ import {
   View,
   Text,
   StatusBar,
-  Button,
-  Alert,
 } from 'react-native';
+ export default class PostSearchView extends BaseComponent {
+  constructor(props) {
+      super(props);
+      this.state = {
+          value: '',
+          placeholder: 'please search here....',
+          isPostList: false, 
+          keyword: '', 
+          searchHistory: [], 
+          hotTagsArr: [], 
+      };
+  }
+  componentDidMount() {
+        this._getHotWords()
+    }
+   
+  _getHotWords() {
+    ModalIndicator.show('请求中...')
+    let params = '{"data":{}}'
+    this._post('../test.json', {},
+        params, true, data => {
+            ModalIndicator.hide()
+            if (data.code == 0) {
+                console.log('hotTagsArr: ' + data.data)
+                console.log('hotTagsArr: ' + data.data.hot_words)
+                if (!_.isEmpty(data.data.hot_words)) {
+                    this.setState({
+                        hotTagsArr: data.data.hot_words,
+                    })
+                } else {
+                    this.setState({
+                        hotTagsArr: [],
+                    })
+                }
+            } else {
+                _showModalMessage(this.props.navigation, data.msg);
+            }
+        }, err => {
+            ModalIndicator.hide()
+            console.log('err: ', err)
+        })
+    }
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>填写订单</Text>
-              <Text style={styles.sectionDescription}>添加就诊人</Text>
-            </View>
-            <View></View>
-            <View>
-              <Button
-                title="Press me"
-                style={styles.btnStyle}
-                onPress={() => Alert.alert('Right button pressed')}
-              />
-            </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+     
+     onChanegeTextKeyword(Val) {
+      let keys = {};
+      //输入的关键字去空格空字符
+      let newVal = Val.replace(/(^\s*)|(\s*$)/g, "")
+      if (!_.isEmpty(newVal)) {
+          keys = {
+              keyword: newVal
+          };
+          this.setState({isPostList: true})
+      } else {
+          Toast.message('请输入搜索关键字', null, 'center')
+      }
+      this.setState({keyword: keys});
+  } 
 
+  
+  _onFous(v) {
+      if (v.nativeEvent.target) {
+          this.setState({isPostList: false})
+      }
+  }
+ 
+  _getHistory() {
+      // 查询本地历史
+      getItems("searchHistory").then(data => {
+          if (data == null) {
+              this.setState({
+                  searchHistory: [],
+              })
+          } else {
+              this.setState({
+                  searchHistory: data,
+              })
+          }
+      })
+  }
+ 
+  render() {
+    return (
+        <View style={styles.container}>
+         
+            <NavigationEvents onWillFocus={() => {
+               
+                this._getHistory();
+            }}/>
+            <Header title='搜索'
+                    navigation={this.props.navigation}
+                    show_close_img={true}
+            />
+            <View style={styles.inputBox}>
+                
+                <TextInput style={styles.inputText}
+                           autoCapitalize="none"
+                           value={this.state.value}
+                           onChangeText={(text) => this.setState({value: text})}
+                           onSubmitEditing={() => {
+                              
+                               this.onChanegeTextKeyword(this.state.value);
+                               
+                           }}
+                           returnKeyType="search"
+                           underlineColorAndroid="transparent"
+                           placeholder={this.state.placeholder}
+                           placeholderTextColor={'#BFBFBF'}
+                           onFocus={this._onFous.bind(this)}
+                           autoFocus={true}
+                           defaultValue={this.state.value}
+                           keyboardType="default"/>
+            </View>
+            <View style={styles.lin}/>
+            {
+                (this.state.isPostList) ?
+                   
+                    <ScrollView style={styles.scrollView}>
+                        <View style={styles.listView}>
+                            <PostSearchList keyword={this.state.keyword}{...this.props}/>
+                        </View>
+                    </ScrollView>
+                    :
+                 
+                    <ScrollView style={styles.scrollView}>
+                      
+                        {
+                           
+                        } 
+                    </ScrollView>
+            }
+        </View>
+    )
+}
+
+}
 const styles = StyleSheet.create({
-  btnStyle: {
-    backgroundColor: 'rgba(187, 187, 187, 100)',
-    wodth: 242,
-    height: 34,
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-    textAlign: 'center',
-    justifyContent: 'center', //主轴方向居中
-    alignItems: 'center', //侧轴方向居中
-  },
-  sectionDescription: {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+},
+inputBox: {
+    height: Platform.OS === 'ios' ? 25 : 35,
+    marginLeft: 15,
+    marginRight: 15,
+    flexDirection: 'row',
+    backgroundColor: '#F4F4F4',
+    borderRadius: 8,
+    borderWidth: 0,
+    marginTop: 10,
+},
+lin: {
+    height: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    backgroundColor: '#f1f1f1',
+    marginTop: 10,
+},
+scrollView: {},
+listView: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 10,
+},
+head1: {
+    paddingHorizontal: 16,
+    marginTop: 18,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+},
+head: {
+    paddingHorizontal: 16,
     marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-    backgroundColor: 'rgba(228, 224, 224, 100)',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+},
+queryList: {
+    marginTop: 10,
+    marginRight: 16,
+    marginLeft: 16,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    backgroundColor: "#fff"
+},
+queryItem: {
+    flex: 1,
+    fontSize: scale(12),
+    backgroundColor: '#F4F4F4',
+    paddingHorizontal: 30,
+    paddingVertical: 6,
+    borderRadius: 3,
+    marginRight: 10,
+    marginBottom: 10
+},
+inputIcon: {
+    margin: Platform.OS === 'ios'
+        ? 5
+        : 10
+},
+inputText: {
+    flex: 1,
+    paddingLeft: 5,
+    textAlignVertical: 'center',
+    paddingVertical: 0
+},
+noData: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    marginTop: 10,
+    marginBottom: 12,
+},
+noDataTxt: {
+    fontSize: scale(12),
+},
 });
 
-export default App;
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# reactnative_project
+
+# How to start the project?
+
+1.  First you should config your jdk and sdk.(Reference the ReactNative official documents.)
+2.  In terminal of this project input "yarn android" to start your procedure while you should open Android Studio.
+3.  Then you can debug your procedure.
+4.  First of all Android Studio is so need storage so you also can download the substrate,because our complany's project using this tool.
+
+# Another
+
+1. For this test because before i used vue in the past,so i didn't do this test very well.
+2. But now and in the furture i am tring to make myself stronger.
+3. Thank you give me the chance.
+4. For react native and react i turst myself i can learn it well and exciting.
